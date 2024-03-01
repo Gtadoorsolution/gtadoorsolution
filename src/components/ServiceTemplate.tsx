8420fe336bff8dd2dd52a98b105acf5a2756bf50
AlecCross <cross2472@gmail.com>
2024-03-01 10:39:22 +0200

    Edit Styles

--- src/components/ServiceTemplate.tsx
+++ src/components/ServiceTemplate.tsx
@@ -3,6 +3,9 @@ import Layout from './Layout'
 import Image from 'next/image'
 import styles from '../styles/page.module.css'
 import Head from 'next/head';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
 
 interface ServiceTemplateProps {
     title: string | undefined;
@@ -23,10 +26,11 @@ const ServiceTemplate: React.FC<ServiceTemplateProps> = ({ title, description, i
             <div className={styles.wrapper}>
                 <div className={styles.imageWrapper}>
                     <Image
-                        className={styles.image}
                         src={image || '/default-ui.webp'}
                         alt={title || 'alt'}
-                        fill
+                        width={300}
+                        height={300}
+                        layout="responsive"
                         sizes="
                                         (max-width: 640px) 100vw,
                                         (max-width: 1024px) 50vw,
@@ -38,47 +42,106 @@ const ServiceTemplate: React.FC<ServiceTemplateProps> = ({ title, description, i
                 </div>
                 <h1 className={styles.title}>{title}</h1>
                 <p className={styles.description}>{description}</p>
-                <h2></h2>
-                <p></p>
+                <h2>Lorem ipsum dolor sit</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. At explicabo sit dicta. Facere blanditiis quam eius molestiae consectetur doloremque voluptates perspiciatis veniam accusantium repellendus, iste culpa assumenda magnam maiores voluptatibus.</p>
                 <div>
-                    {/* <Image
-                    className={styles.image}
-                    src={service.image}
-                    alt={service.title}
-                    fill
+                    <Swiper
+                        modules={[Navigation, Pagination, Scrollbar, A11y]}
+                        spaceBetween={0}
+                        navigation
+                        pagination={{ clickable: true }}
+                        scrollbar={{ draggable: true }}
+                        onSwiper={(swiper) => console.log(swiper)}
+                        onSlideChange={() => console.log('slide change')}
+                        breakpoints={{
+                            500: {
+                                slidesPerView: 1,
+                            },
+                            768: {
+                                slidesPerView: 2,
+                            },
+                            1200: {
+                                slidesPerView: 3,
+                            },
+                        }}
+                    >
+                        <SwiperSlide>
+                            <Image
+                                src={"/default-ui.webp"}
+                                alt={"default-ui.webp"}
+                                width={600}
+                                height={419}
+                                layout="responsive"
+                                sizes="
+                                        (max-width: 640px) 100vw,
+                                        (max-width: 1024px) 50vw,
+                                        33vw
+                                        "
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Image
+                                src={"/default-ui.webp"}
+                                alt={"default-ui.webp"}
+                                width={600}
+                                height={419}
+                                layout="responsive"
+                                sizes="
+                                        (max-width: 640px) 100vw,
+                                        (max-width: 1024px) 50vw,
+                                        33vw
+                                        "
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Image
+                                src={"/default-ui.webp"}
+                                alt={"default-ui.webp"}
+                                width={600}
+                                height={419}
+                                layout="responsive"
+                                sizes="
+                                        (max-width: 640px) 100vw,
+                                        (max-width: 1024px) 50vw,
+                                        33vw
+                                        "
+                            />
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <Image
+                                src={"/default-ui.webp"}
+                                alt={"default-ui.webp"}
+                                width={600}
+                                height={419}
+                                layout="responsive"
+                                sizes="
+                                        (max-width: 640px) 100vw,
+                                        (max-width: 1024px) 50vw,
+                                        33vw
+                                        "
+                            />
+                        </SwiperSlide>
+                    </Swiper>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, repellat earum atque, alias, voluptatibus voluptate laborum officiis molestiae fuga esse fugit odit aliquid quae mollitia nihil? Natus repellat nam ipsa!</p>
+                <h3>Lorem ipsum</h3>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facilis voluptas consequatur veniam non saepe quod mollitia aliquid debitis quaerat, similique animi ratione autem voluptates. Temporibus sequi consequatur recusandae minima facere!</p>
+                <Image
+                    // style={{paddingBottom: 100}}
+                    // className={styles.image}
+                    src={"/default-ui.webp"}
+                    alt={"default-ui.webp"}
+                    width={600}
+                    height={419}
+                    layout="responsive"
+                    // fill
                     sizes="
                                         (max-width: 640px) 100vw,
                                         (max-width: 1024px) 50vw,
                                         33vw
                                         "
                 />
-                <Image
-                    className={styles.image}
-                    src={service.image}
-                    alt={service.title}
-                    fill
-                    sizes="
-                                        (max-width: 640px) 100vw,
-                                        (max-width: 1024px) 50vw,
-                                        33vw
-                                        "
-                /> */}
-                </div>
-                <p></p>
-                <h3></h3>
-                <p></p>
-                {/* <Image
-                className={styles.image}
-                src={service.image}
-                alt={service.title}
-                fill
-                sizes="
-                                        (max-width: 640px) 100vw,
-                                        (max-width: 1024px) 50vw,
-                                        33vw
-                                        "
-            /> */}
-                <p></p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit saepe voluptas itaque fuga accusamus sed natus, voluptate facere. Quas voluptatum facilis culpa aspernatur perferendis veritatis cumque pariatur minima excepturi vero?</p>
             </div>
         </Layout>
     )
