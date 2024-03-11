63df5f31aee98d0655b55cf41be7821b006aa718
AlecCross <cross2472@gmail.com>
2024-03-11 12:38:24 +0200

    Add google maps

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -19,6 +19,11 @@ const ContactPage = () => {
                     <p>{contacts.telNumber || ""}</p>
                     <p>{contacts.email || ""}</p>
                 </div>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d437135.072632405274!2d-75.72637343453277!3d45.41930932216773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce05b25f5113af%3A0x8a6a51e131dd15ed!2z0J7RgtGC0LDQstCwLCDQntC90YLQsNGA0ZbQviwg0JrQsNC90LDQtNCw!5e1!3m2!1suk!2sua!4v1710153242810!5m2!1suk!2sua" 
+                height="450" 
+                style={{border:0, width: "100%"}}
+                loading="lazy" 
+                ></iframe>
             </div>
         </Layout>
     )
