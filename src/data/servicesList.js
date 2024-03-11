1c877d07b9f88b3dfac76359d0f7064bddae10fd
AlecCross <cross2472@gmail.com>
2024-03-11 12:33:31 +0200

    Change PageContainer css, add short servises dlex description

--- src/data/servicesList.js
+++ src/data/servicesList.js
@@ -1,55 +1,55 @@
 const servicesList = [
     {
         title: "Locksmith Services",
-        description: "We have been proudly serving the GTA for over 20 years, and over time, we have become known for our fast, reliable and effective locksmith services.",
+        description: "Locksmith Services description",
         link: "/services/locksmith-services/",
         image: "/services/Locksmith-Service.jpg"
     },
     {
         title: "Aluminum Storefronts",
-        description: "Designed and used for schools, retail stores, offices and any commercial buildings, these doors are professionally installed to guarantee top quality safety and security to your storefront or building.",
+        description: "Aluminum Storefronts description",
         link: "/services/aluminum-storefronts/",
         image: "/services/Aluminum-Storefronts.jpg"
     },
     {
         title: "Access Control System",
-        description: "Popular for tracking employee attendance or traffic and ideal for providing added levels of security for sensitive documents, these systems can offer state-of-the-art technology with easy-to-use features.",
+        description: "Access Control System description",
         link: "/services/access-control-system/",
         image: "/services/Access-control-system.jpg"
     },
     {
         title: "Automatic Doors",
-        description: "Make a good first impression to visitors of your business with automatic doors. Enjoy the convenience and ease of these doors, while also enjoying the benefits they offer.",
+        description: "Automatic Doors description",
         link: "/services/automatic-doors/",
         image: "/services/Automatic-door-operators.jpg"
     },
     {
         title: "Commercial Door Repair",
-        description: "When damage occurs, our professional team will be out as soon as possible to assess the damage and offer you the best solutions in order to rectify the problem with little disruption to your operation.",
+        description: "Commercial Door Repair description",
         link: "/services/commercial-door-repair/",
         image: "/services/Commercial Door Repair.jpg"
     },
     {
         title: "Metal FireRated Doors",
-        description: "Fire-rated doors play a major role in reducing damage caused by fire. These doors provide maximum safety to a property when fires occur. Not all fire-rated doors are created equal.",
+        description: "Metal FireRated Doors description",
         link: "/services/metal-fire-rated-doors",
         image: "/services/Metal FireRated Doors.jpg"
     },
     {
         title: "Garage Doors",
-        description: "Garage Doors. Lorem ipsum",
+        description: "Garage Doors description",
         link: "/services/metal-fire-rated-doors",
         image: "/services/garage-doors.jpg"
     },
     {
         title: "Front Entry Doors",
-        description: "Many clients prefer the look of wood doors, making this a popular option without having to compromise on quality , durability or fire rating.",
+        description: "Front Entry Doors description",
         link: "/services/front-entry-doors/",
         image: "/services/Front Entry Doors.jpg"
     },
     {
         title: "Emergency Lockout",
-        description: "Many clients prefer the look of wood doors, making this a popular option without having to compromise on quality , durability or fire rating.",
+        description: "Emergency Lockout description",
         link: "/services/emergency-lockout/",
         image: "/services/Emergency Lockout.jpg"
     },
