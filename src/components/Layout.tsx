c838cfe2278cf2114e58ef33c440cd33e732b560
AlecCross <cross2472@gmail.com>
2024-03-11 15:52:09 +0200

    Add test Crisp Chat

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,6 +5,7 @@ import HeaderContacts from './HeaderContacts';
 import UnderFooter from './UnderFooter';
 import styles from '../page.module.css'
 import PageContainer from './PageContainer';
+import Head from 'next/head';
 
 interface LayoutProps {
     children: ReactNode; // Використовуйте тип ReactNode для властивості children
@@ -14,6 +15,23 @@ export default function Layout({ children }: LayoutProps) {
     return (
         <main className={styles.main}>
             {/* <HeaderContacts /> */}
+            <Head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+            window.$crisp=[];
+            window.CRISP_WEBSITE_ID="${process.env.CRISP_WEBSITE_ID}";
+            (function(){
+              var d=document;
+              var s=d.createElement("script");
+              s.src="https://client.crisp.chat/l.js";
+              s.async=1;
+              d.getElementsByTagName("head")[0].appendChild(s);
+            })();
+          `,
+                    }}
+                />
+            </Head>
             <Navbar />
             <PageContainer>
                 <div>{children}</div>
