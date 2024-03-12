790fd9bc72a30fdd02eac5e48cd672e7647b5dde
AlecCross <cross2472@gmail.com>
2024-03-12 10:27:53 +0200

    Fix crisp chat invalid website message

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,43 +1,22 @@
 import React, { ReactNode } from 'react';
 import Navbar from './NavBar';
 import Footer from './Footer';
-import HeaderContacts from './HeaderContacts';
-import UnderFooter from './UnderFooter';
 import styles from '../page.module.css'
 import PageContainer from './PageContainer';
-import Head from 'next/head';
 
 interface LayoutProps {
     children: ReactNode; // Використовуйте тип ReactNode для властивості children
 }
 
 export default function Layout({ children }: LayoutProps) {
+    
     return (
         <main className={styles.main}>
-            {/* <HeaderContacts /> */}
-            <Head>
-                <script
-                    dangerouslySetInnerHTML={{
-                        __html: `
-            window.$crisp=[];
-            window.CRISP_WEBSITE_ID="${process.env.CRISP_WEBSITE_ID}";
-            (function(){
-              var d=document;
-              var s=d.createElement("script");
-              s.src="https://client.crisp.chat/l.js";
-              s.async=1;
-              d.getElementsByTagName("head")[0].appendChild(s);
-            })();
-          `,
-                    }}
-                />
-            </Head>
             <Navbar />
             <PageContainer>
                 <div>{children}</div>
             </PageContainer>
             <Footer />
-            {/* <UnderFooter /> */}
         </main>
     );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
+import dynamic from 'next/dynamic'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// Динамічний імпорт компонента CrispChat з виключенням SSR
+const CrispWithNoSSR = dynamic(() => import('../components/crisp'), {
+  ssr: false,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <CrispWithNoSSR />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
