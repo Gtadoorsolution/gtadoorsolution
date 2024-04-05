0ad6ebad2a72ced3ce30a1bf7fcedc72642e61b3
AlecCross <cross2472@gmail.com>
2024-04-05 20:17:31 +0300

    Change map wiev

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import contacts from '../data/contacts'
 import styles from '../styles/contact.module.css'
 import Head from 'next/head';
+import Image from 'next/image';
 
 const ContactPage = () => {
     return (
@@ -18,12 +19,7 @@ const ContactPage = () => {
                     <p>tel: {contacts.telNumber || ""}</p>
                     <p>email: {contacts.email || ""}</p>
                 </div>
-                <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d621583.5473622468!2d-79.6638166550695!3d43.63844929862597!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sca!4v1710359706059!5m2!1sen!2sca"
-                height="450" 
-                loading="lazy"
-                className={styles.map}
-                ></iframe>
+                <Image className={styles.map} src="/Service area1.webp" alt="map" width={1024} height={453}/>
             </div>
         </Layout>
     )
