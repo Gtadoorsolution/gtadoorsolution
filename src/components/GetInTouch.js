77a94dc3bca66e85dd8c196dd942bcbb73304503
AlecCross <cross2472@gmail.com>
2024-04-07 13:53:27 +0300

    Fix getInTouch styles

--- src/components/GetInTouch.js
+++ src/components/GetInTouch.js
@@ -86,15 +86,6 @@ const GetInTouch = () => {
                         placeholder={values.subject ? "" : "Subject"}
                         required
                     />
-                    <textarea
-                        className={`${getInTouchStyles.input} ${touched.message && !values.message ? getInTouchStyles.errorInput : ''}`}
-                        name="message"
-                        rows="4"
-                        value={values.message}
-                        onChange={handleChange}
-                        onBlur={onBlur}
-                        placeholder={values.message ? "" : "\nAdditional Message\n(Not Required)"}
-                    ></textarea>
                     <div>
                         <button
                             className={getInTouchStyles.button}
@@ -111,9 +102,9 @@ const GetInTouch = () => {
                     </div>
                 </form>
             </div>
-            <div>
+            <div className={getInTouchStyles.contactWrap}>
                 <p className={getInTouchStyles.title}>Contact Info</p>
-                <ul className={getInTouchStyles.contactWrap}>
+                <ul>
                     <li className={getInTouchStyles.contact}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#835c46" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
