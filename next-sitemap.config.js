16d1a40da2b2624cee5b1b5f7d860fbf735b359a
Gtadoorsolution <gtadoorsolution@gmail.com>
2024-09-10 14:58:13 -0800

    SEO add sitemap and robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.gtadoorsolutions.ca', // Змініть на URL вашого сайту
+    generateRobotsTxt: true, // Генерує robots.txt разом із sitemap.xml
+  }
+  
