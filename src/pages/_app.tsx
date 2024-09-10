15da0da7f0ef2030db8c3a15a3f745ec8e39d6da
Gtadoorsolution <gtadoorsolution@gmail.com>
2024-09-10 15:37:53 -0800

    SEO remove resource that block this pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,9 @@
 import dynamic from 'next/dynamic'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import redirectMissingCustomer from '../middleware/redirectMissingCustomer';
-import { NextPageContext } from 'next';
 import FloatingButton from '@/components/FloatingButton';
 import CrispFloatingButton from '@/components/CrispFloatingButton';
 
-
-// Динамічний імпорт компонента CrispChat з виключенням SSR
 const CrispWithNoSSR = dynamic(() => import('../components/crisp'), {
   ssr: false,
 });
@@ -24,15 +20,5 @@ function MyApp({ Component, pageProps }: AppProps) {
     </>
   );
 }
-
-MyApp.getInitialProps = async ({ Component, ctx }: { Component: any, ctx: NextPageContext }) => {
-  // Викликаємо middleware для перевірки статусу сайту
-  await redirectMissingCustomer(ctx.req, ctx.res);
-
-  // Визначений компонент буде рендеритися лише якщо статус сайту не maintenance
-  const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
-
-  return { pageProps };
-};
 
 export default MyApp;
