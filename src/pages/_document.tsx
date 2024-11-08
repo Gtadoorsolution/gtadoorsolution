7e0aca80185a6ad7ae75461adfd1054c7efc02c3
AlecCross <cross2472@gmail.com>
2024-11-08 23:16:59 +0200

    Add ADS tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,22 @@ class MyDocument extends Document {
         return (
             <Html lang="en">
                 <Head>
+                    <script
+                        async
+                        src="https://www.googletagmanager.com/gtag/js?id=AW-16764764761"
+                    ></script>
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-16764764761');
+      `,
+                        }}
+                    />
+                    <title>GTADOORSOLUTION</title>
+                    <meta name="description" content="MAKING SURE THE KEYS ARE IN YOUR HANDS" />
                     <meta property="og:image" content="/LogoBaner.svg" />
                 </Head>
                 <body>
