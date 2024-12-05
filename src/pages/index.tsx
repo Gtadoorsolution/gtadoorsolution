c5633c4d9551356685cc5b4e1bfcf72695581276
root <root@DESKTOP-NTJQST3.localdomain>
2024-12-05 20:19:10 +0000

    change image for og tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,13 +11,13 @@ const Home: NextPage = () => {
         <meta name="description" content="MAKING SURE THE KEYS ARE IN YOUR HANDS" />
         <meta property="og:title" content="GTADOORSOLUTIONS" />
         <meta property="og:description" content="MAKING SURE THE KEYS ARE IN YOUR HANDS" />
-        <meta property="og:image" content="https://gtadoorsolutions.ca/LogoBaner.png" />
+        <meta property="og:image" content="https://gtadoorsolutions.ca/open-graph-logo.jpg" />
         <meta property="og:url" content="https://gtadoorsolutions.ca/" />
         <meta property="og:type" content="website" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="GTADOORSOLUTIONS" />
         <meta name="twitter:description" content="MAKING SURE THE KEYS ARE IN YOUR HANDS" />
-        <meta name="twitter:image" content="https://gtadoorsolutions.ca/LogoBaner.png" />
+        <meta name="twitter:image" content="https://gtadoorsolutions.ca/open-graph-logo.jpg" />
         <meta name="twitter:url" content="https://gtadoorsolutions.ca/" />
         <link rel="canonical" href="https://gtadoorsolutions.ca" />
       </Head>
